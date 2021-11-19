2061101bda2b3dbc8e11f51cf5f0abe784df60af
David Rodríguez <deivid.rodriguez@riseup.net>
2021-11-20 00:04:12 +0100

    Don't overwrite locked sources with Gemfile sources
    The resolver will already set the proper source for each locked spec
    once sources have been resolved.
    
    This change makes things go back to the more conservative bundler pre
    2.2.18 behaviour, which is what's generally expected for `bundle
      install` (as opposed to `bundle update`).

--- bundler/lib/bundler/definition.rb
+++ bundler/lib/bundler/definition.rb
@@ -163,10 +163,6 @@ module Bundler
       end
     end
 
-    def multisource_allowed?
-      @multisource_allowed
-    end
-
     def resolve_only_locally!
       @remote = false
       sources.local_only!
@@ -718,11 +714,6 @@ module Bundler
           deps << dep
         end
 
-        s.source = (dep && dep.source) || sources.get(s.source) unless multisource_allowed?
-
-        # Don't add a spec to the list if its source is expired. For example,
-        # if you change a Git gem to RubyGems.
-        next if s.source.nil?
         next if @unlock[:sources].include?(s.source.name)
 
         # If the spec is from a path source and it doesn't exist anymore
